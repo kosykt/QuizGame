f371662b35557419fdd0b7e66e8eac97e6c078f4
kosykt <kosvasl09@gmail.com>
2021-03-12 19:21:16 +0900

    button_start, levels_text

---
+++ app/src/main/java/ykt/kos/quizgame/GameLevels.java
@@ -0,0 +1,19 @@
+package ykt.kos.quizgame;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameLevels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_levels);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
